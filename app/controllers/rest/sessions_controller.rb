8b4b7209bac7621b87a0e2afbc93a41f18789d98
jisanchezp <juanisanchez@outlook.com>
2018-01-20 15:03:38 -0300

    Add logic to verify endpoint. Add verify helper to handle responses

--- app/controllers/rest/sessions_controller.rb
+++ app/controllers/rest/sessions_controller.rb
@@ -1,8 +1,14 @@
 class Rest::SessionsController < ActionController::API
+  include Rest::SessionsHelper
 
   def verify
-    render json: { status: 200,
-      message: "OK"
+    email = params[:email]
+    image = params[:image]
+
+    validation_response = verify_session(email, image)
+
+    render json: { status: validation_response[:status],
+      message: validation_response[:message]
     }.to_json
   end
 end
--- app/helpers/rest/sessions_helper.rb
+++ app/helpers/rest/sessions_helper.rb
@@ -1,2 +1,28 @@
 module Rest::SessionsHelper
+
+  def verify_session(email, image)
+
+    User.all.each do |user|
+      if user.email.downcase == email.downcase
+        if user.image == image
+          return response_hash(false)
+        else
+          return response_hash(true)
+        end
+      end
+    end
+
+    return response_hash(false)
+  end
+
+
+  private
+
+    def response_hash(isAuthorized)
+      if !isAuthorized
+        return {status: 401, message: "No Autorizado"}
+      end
+
+      return {status: 200, message: "Ok"}
+    end
 end
